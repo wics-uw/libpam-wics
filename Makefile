2070d25dab1da58dfe3e82d6e03ac9c491969111
Elana Hashman <elana@hashman.ca>
2015-12-04 18:33:01 -0500

    Fork libpam-csc to libpam-wics

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@ CC=gcc
 CFLAGS=-std=c99 -g -O2 -fPIC -Wall -DLDAP_DEPRECATED
 LDFLAGS=-g -shared
 
-all: pam_csc.so
+all: pam_wics.so
 
-pam_csc.so: pam_csc.o
+pam_wics.so: pam_wics.o
 	$(CC) -o $@ $(LDFLAGS) $< -lpam -lldap
 
 clean:
-	rm -f pam_csc.so pam_csc.o
+	rm -f pam_wics.so pam_wics.o
 
 install:
-	cp pam_csc.so /lib/security/
+	cp pam_wics.so /lib/security/
--- pam_csc.c
+++ pam_csc.c
@@ -28,24 +28,22 @@
 #  define PAM_EXTERN extern
 #endif
 
-#define PAM_CSC_CSC_BASE_DN         "ou=People,dc=csclub,dc=uwaterloo,dc=ca"
-#define PAM_CSC_LDAP_TIMEOUT        5
-#define PAM_CSC_ALLOWED_USERNAMES   {"nobody"}
-#define PAM_CSC_EXPIRED_MSG \
+#define PAM_WICS_WICS_BASE_DN         "ou=People,dc=wics,dc=uwaterloo,dc=ca"
+#define PAM_WICS_LDAP_TIMEOUT        5
+#define PAM_WICS_ALLOWED_USERNAMES   {"nobody"}
+#define PAM_WICS_EXPIRED_MSG \
     "*****************************************************************************\n" \
-    "*                                                                           *\n" \
-    "*    Your account has expired - please contact the Computer Science Club    *\n" \
-    "*                                                                           *\n" \
+    "*   Your account has expired. Please contact the WiCS Systems Committee.    *\n" \
     "*****************************************************************************\n"
 
-#define PAM_CSC_SYSLOG_EXPIRED_NO_TERMS \
-    "(pam_csc): %s was not registered for current term or previous term - denying login\n"
-#define PAM_CSC_SYSLOG_EXPIRED_LAST_TERM \
-    "(pam_csc): %s was not registered for current term but was registered for previous term - permitting login\n"
-#define PAM_CSC_SYSLOG_NOT_A_MEMBER \
-    "(pam_csc): %s is not a member account - permitting login\n"
-#define PAM_CSC_SYSLOG_SASL_UNRECOGNIZED_CALLBACK \
-    "(pam_csc): %ld is not a recognized SASL callback option\n"
+#define PAM_WICS_SYSLOG_EXPIRED_NO_TERMS \
+    "(pam_wics): %s was not registered for current term or previous term - denying login\n"
+#define PAM_WICS_SYSLOG_EXPIRED_LAST_TERM \
+    "(pam_wics): %s was not registered for current term but was registered for previous term - permitting login\n"
+#define PAM_WICS_SYSLOG_NOT_A_MEMBER \
+    "(pam_wics): %s is not a member account - permitting login\n"
+#define PAM_WICS_SYSLOG_SASL_UNRECOGNIZED_CALLBACK \
+    "(pam_wics): %ld is not a recognized SASL callback option\n"
 
 /*
  * User terms are defined as (3 * year + term) where term is:
@@ -55,7 +53,7 @@
 
 #define HANDLE_WARN \
 { \
-    syslog(LOG_AUTHPRIV | LOG_WARNING, "pam_csc generated a warning on line %d of %s\n", __LINE__, __FILE__); \
+    syslog(LOG_AUTHPRIV | LOG_WARNING, "pam_wics generated a warning on line %d of %s\n", __LINE__, __FILE__); \
     retval = PAM_SUCCESS; \
     goto cleanup; \
 }
@@ -72,17 +70,17 @@
 #define WARN_LDAP(x) \
     if( (x) != LDAP_SUCCESS ) HANDLE_WARN
 
-struct pam_csc_sasl_interact_param
+struct pam_wics_sasl_interact_param
 {
     const char* realm;
     const char* user;
     char pass[100];
 };
-typedef struct pam_csc_sasl_interact_param pam_csc_sasl_interact_param_t;
+typedef struct pam_wics_sasl_interact_param pam_wics_sasl_interact_param_t;
 
-int pam_csc_sasl_interact(LDAP* ld, unsigned flags, void* def, void* inter)
+int pam_wics_sasl_interact(LDAP* ld, unsigned flags, void* def, void* inter)
 {
-    pam_csc_sasl_interact_param_t* param = (pam_csc_sasl_interact_param_t*)def;
+    pam_wics_sasl_interact_param_t* param = (pam_wics_sasl_interact_param_t*)def;
     sasl_interact_t* interact = (sasl_interact_t*)interact;
     while(interact->id != SASL_CB_LIST_END)
     {
@@ -99,7 +97,7 @@ int pam_csc_sasl_interact(LDAP* ld, unsigned flags, void* def, void* inter)
             break;
         default:
             syslog(LOG_AUTHPRIV | LOG_NOTICE,
-                PAM_CSC_SYSLOG_SASL_UNRECOGNIZED_CALLBACK, interact->id);
+                PAM_WICS_SYSLOG_SASL_UNRECOGNIZED_CALLBACK, interact->id);
             interact->result = "";
             break;
         }
@@ -109,7 +107,7 @@ int pam_csc_sasl_interact(LDAP* ld, unsigned flags, void* def, void* inter)
     return LDAP_SUCCESS;
 }
 
-char* pam_csc_escape_ldap_string(const char* src)
+char* pam_wics_escape_ldap_string(const char* src)
 {
     char *dst, *dst_ptr;
     int i;
@@ -133,7 +131,7 @@ char* pam_csc_escape_ldap_string(const char* src)
     return dst;
 }
 
-int pam_csc_print_message(pam_handle_t* pamh, char* msg, int style)
+int pam_wics_print_message(pam_handle_t* pamh, char* msg, int style)
 {
     int retval = PAM_SUCCESS;
     const struct pam_conv* conv;
@@ -162,22 +160,22 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const c
     const char* username;
     struct passwd* pwd;
     struct group *grp;
-    const char* allowed_usernames[] = PAM_CSC_ALLOWED_USERNAMES;
+    const char* allowed_usernames[] = PAM_WICS_ALLOWED_USERNAMES;
     unsigned int i;
     time_t cur_time;
     struct tm* local_time;
     int long_term, term_month;
     static const char term_chars[] = {'w', 's', 'f'};
     char cur_term[6], prev_term[6];
-    LDAP *ld_csc = NULL;
+    LDAP *ld_wics = NULL;
     char* username_escaped = NULL;
-    char *filter_csc = NULL;
-    char *attrs_csc[] = {"objectClass", "term", "nonMemberTerm", NULL};
+    char *filter_wics = NULL;
+    char *attrs_wics[] = {"objectClass", "term", "nonMemberTerm", NULL};
     bool expired, syscom = 0; 
     const char* pam_rhost;
-    int msg_csc;
-    LDAPMessage *res_csc = NULL;
-    struct timeval timeout = {PAM_CSC_LDAP_TIMEOUT, 0};
+    int msg_wics;
+    LDAPMessage *res_wics = NULL;
+    struct timeval timeout = {PAM_WICS_LDAP_TIMEOUT, 0};
     LDAPMessage* entry = NULL;
     char **values = NULL, **nmvalues = NULL, **values_iter = NULL;
 
@@ -217,7 +215,7 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const c
     }
 
     /* escape username */
-    WARN_ZERO( username_escaped = pam_csc_escape_ldap_string(username) );
+    WARN_ZERO( username_escaped = pam_wics_escape_ldap_string(username) );
 
     /* get term info and compute current and previous term */
     WARN_NEG1( cur_time = time(NULL) )
@@ -228,46 +226,40 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const c
     sprintf(prev_term, "%c%d", term_chars[long_term % 3], long_term / 3);
     term_month = local_time->tm_mon % 4;
 
-    /* connect to CSC */
-    WARN_LDAP( ldap_create(&ld_csc) )
-    WARN_NEG1( ldap_simple_bind(ld_csc, NULL, NULL) )
+    /* connect to WICS */
+    WARN_LDAP( ldap_create(&ld_wics) )
+    WARN_NEG1( ldap_simple_bind(ld_wics, NULL, NULL) )
 
-    /* check if we are logging in from a CSCF teaching thin client */
-    if(pam_get_item(pamh, PAM_RHOST, (const void**)&pam_rhost) && pam_rhost)
-    {
-        /* TODO: check if pam_rhost is tcNNN.student.cs */
-    }
+    /* create WICS request string */
+    WARN_ZERO( filter_wics = malloc(140 + strlen(username_escaped)) )
+    sprintf(filter_wics, "(&(uid=%s)(|(&(objectClass=member)(|(term=%s)(term=%s)(nonMemberTerm=%s)(nonMemberTerm=%s)))(!(objectClass=member))))", username_escaped, cur_term, prev_term, cur_term, prev_term);
 
-    /* create CSC request string */
-    WARN_ZERO( filter_csc = malloc(140 + strlen(username_escaped)) )
-    sprintf(filter_csc, "(&(uid=%s)(|(&(objectClass=member)(|(term=%s)(term=%s)(nonMemberTerm=%s)(nonMemberTerm=%s)))(!(objectClass=member))))", username_escaped, cur_term, prev_term, cur_term, prev_term);
+    /* issue WICS request */
+    WARN_NEG1( msg_wics = ldap_search(ld_wics, PAM_WICS_WICS_BASE_DN,
+        LDAP_SCOPE_SUBTREE, filter_wics, attrs_wics, 0) )
 
-    /* issue CSC request */
-    WARN_NEG1( msg_csc = ldap_search(ld_csc, PAM_CSC_CSC_BASE_DN,
-        LDAP_SCOPE_SUBTREE, filter_csc, attrs_csc, 0) )
+    /* wait for WICS response */
+    WARN_NEG1( ldap_result(ld_wics, msg_wics, 1, &timeout, &res_wics) )
 
-    /* wait for CSC response */
-    WARN_NEG1( ldap_result(ld_csc, msg_csc, 1, &timeout, &res_csc) )
-
-    /* check if we received an entry from CSC */
-    if(ldap_count_entries(ld_csc, res_csc) == 0)
+    /* check if we received an entry from WICS */
+    if(ldap_count_entries(ld_wics, res_wics) == 0)
     {
         /* show notice and disallow login */
-        pam_csc_print_message(pamh, PAM_CSC_EXPIRED_MSG, PAM_ERROR_MSG);
-        syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_CSC_SYSLOG_EXPIRED_NO_TERMS,
+        pam_wics_print_message(pamh, PAM_WICS_EXPIRED_MSG, PAM_ERROR_MSG);
+        syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_WICS_SYSLOG_EXPIRED_NO_TERMS,
             username);
         retval = (syscom ? PAM_SUCCESS : PAM_AUTH_ERR);
         goto cleanup;
     }
 
-    /* get CSC entry */
-    WARN_ZERO( entry = ldap_first_entry(ld_csc, res_csc) )
-    values = ldap_get_values(ld_csc, entry, "term");
-    nmvalues = ldap_get_values(ld_csc, entry, "nonMemberTerm");
+    /* get WICS entry */
+    WARN_ZERO( entry = ldap_first_entry(ld_wics, res_wics) )
+    values = ldap_get_values(ld_wics, entry, "term");
+    nmvalues = ldap_get_values(ld_wics, entry, "nonMemberTerm");
 
     if(!values && !nmvalues)
     {
-        syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_CSC_SYSLOG_NOT_A_MEMBER,
+        syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_WICS_SYSLOG_NOT_A_MEMBER,
             username);
         retval = PAM_SUCCESS;
         goto cleanup;
@@ -306,15 +298,15 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const c
         if(term_month == 0)
         {
             /* show notice and continue */
-            pam_csc_print_message(pamh, PAM_CSC_EXPIRED_MSG, PAM_TEXT_INFO);
-            syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_CSC_SYSLOG_EXPIRED_LAST_TERM,
+            pam_wics_print_message(pamh, PAM_WICS_EXPIRED_MSG, PAM_TEXT_INFO);
+            syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_WICS_SYSLOG_EXPIRED_LAST_TERM,
                 username);
         }
         else
         {
             /* show notice and disallow login */
-            pam_csc_print_message(pamh, PAM_CSC_EXPIRED_MSG, PAM_ERROR_MSG);
-            syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_CSC_SYSLOG_EXPIRED_NO_TERMS,
+            pam_wics_print_message(pamh, PAM_WICS_EXPIRED_MSG, PAM_ERROR_MSG);
+            syslog(LOG_AUTHPRIV | LOG_NOTICE, PAM_WICS_SYSLOG_EXPIRED_NO_TERMS,
                 username);
             retval = (syscom ? PAM_SUCCESS : PAM_AUTH_ERR);
         }
@@ -323,9 +315,9 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const c
 cleanup:
     if(values) ldap_value_free(values);
     if(nmvalues) ldap_value_free(nmvalues);
-    if(res_csc) ldap_msgfree(res_csc);
-    if(ld_csc) ldap_unbind(ld_csc);
-    if(filter_csc) free(filter_csc);
+    if(res_wics) ldap_msgfree(res_wics);
+    if(ld_wics) ldap_unbind(ld_wics);
+    if(filter_wics) free(filter_wics);
     if(username_escaped) free(username_escaped);
     return retval;
 }
